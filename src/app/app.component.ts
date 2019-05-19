b2db959bacd61c4fcbcf41f9cab52603e2e00e50
Matt Reed <matthewreed26@gmail.com>
2019-05-18 20:47:17 -0400

    video; update tar dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chroma-video';
+  title = 'Chroma Video';
+  @ViewChild('video') video: ElementRef;
+  private stream: MediaStream = null;
+  // Get video
+  constructor() {
+    navigator.mediaDevices.getUserMedia({video: true})
+      .then((stream) => {
+        const videoElement = this.video.nativeElement;
+        videoElement.srcObject = stream;
+        videoElement.onloadedmetadata = (e: any) => {
+          videoElement.play();
+        };
+      })
+      .catch((error) => {
+          console.log('Error:', error);
+      });
+  }
 }

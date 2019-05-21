e25127b40de40e3f58bf086add3ee9ba06e9d8cd
Matt Reed <matthewreed26@gmail.com>
2019-05-20 20:27:01 -0400

    removes blue

--- src/app/video-canvas/video-canvas.component.ts
+++ src/app/video-canvas/video-canvas.component.ts
@@ -20,7 +20,27 @@ export class VideoCanvasComponent implements OnInit {
 
   draw(playerElement: HTMLCanvasElement, playerCtx: CanvasRenderingContext2D) {
     playerCtx.drawImage(this.videoStreamerService.getImageSource(), 0, 0, playerElement.width, playerElement.height);
+
+    const frame = playerCtx.getImageData(0, 0, playerElement.width, playerElement.height);
+    playerCtx.putImageData(this.findAndRemoveBlue(frame), 0, 0);
+
     window.requestAnimationFrame(() => this.draw(playerElement, playerCtx));
+  }
+
+  findAndRemoveBlue(frame: ImageData): ImageData {
+    const l = frame.data.length / 4;
+    for (let i = 0; i < l; i++) {
+      const r = frame.data[i * 4 + 0];
+      const g = frame.data[i * 4 + 1];
+      const b = frame.data[i * 4 + 2];
+
+      if (b > r && b > g) {
+        if (b > 100 ) {
+          frame.data[i * 4 + 3] = 0;
+        }
+      }
+    }
+    return frame;
   }
 
 }

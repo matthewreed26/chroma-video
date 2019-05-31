d88f33cf0afe9dc177f1130d07af7821bcb8430a
Matt Reed <matthewreed26@gmail.com>
2019-05-31 15:28:26 -0400

    front primary canvas is now not pink

--- src/app/chroma-rotoscoping/chroma-rotoscoping.component.ts
+++ src/app/chroma-rotoscoping/chroma-rotoscoping.component.ts
@@ -21,21 +21,21 @@ export class ChromaRotoscopingComponent implements OnInit {
     const ctx1: CanvasRenderingContext2D = c1.getContext('2d');
     const ctx2: CanvasRenderingContext2D = c2.getContext('2d');
     const ctx3: CanvasRenderingContext2D = c3.getContext('2d');
-    this.ngZone.runOutsideAngular(() => this.draw(c1, ctx1));
-    this.ngZone.runOutsideAngular(() => this.draw(c2, ctx2));
-    this.ngZone.runOutsideAngular(() => this.draw(c3, ctx3));
+    this.ngZone.runOutsideAngular(() => this.draw(c1, ctx1, true));
+    this.ngZone.runOutsideAngular(() => this.draw(c2, ctx2, false));
+    this.ngZone.runOutsideAngular(() => this.draw(c3, ctx3, false));
   }
 
-  draw(canvasElement: HTMLCanvasElement, canvasCtx: CanvasRenderingContext2D) {
+  draw(canvasElement: HTMLCanvasElement, canvasCtx: CanvasRenderingContext2D, primaryCanvas: boolean) {
     canvasCtx.drawImage(this.videoStreamerService.getImageSource(), 0, 0, canvasElement.width, canvasElement.height);
 
     const frame = canvasCtx.getImageData(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.putImageData(this.findAndRemoveBlue(frame), 0, 0);
+    canvasCtx.putImageData(this.findAndRemoveBlue(frame, primaryCanvas), 0, 0);
 
-    window.requestAnimationFrame(() => this.draw(canvasElement, canvasCtx));
+    window.requestAnimationFrame(() => this.draw(canvasElement, canvasCtx, primaryCanvas));
   }
 
-  findAndRemoveBlue(frame: ImageData): ImageData {
+  findAndRemoveBlue(frame: ImageData, primaryCanvas: boolean): ImageData {
     const l = frame.data.length / 4;
     for (let i = 0; i < l; i++) {
       const r = frame.data[i * 4 + 0];
@@ -47,8 +47,10 @@ export class ChromaRotoscopingComponent implements OnInit {
           frame.data[i * 4 + 3] = 0;
         }
       } else {
-        frame.data[i * 4 + 0] = 200;
-        frame.data[i * 4 + 2] = 200;
+        if (!primaryCanvas) {
+          frame.data[i * 4 + 0] = 200;
+          frame.data[i * 4 + 2] = 200;
+        }
       }
     }
     return frame;
